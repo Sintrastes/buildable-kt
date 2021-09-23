64485337910d4c7cd048dfcad99bac4e370ca6bd
Nathan Bedell <nbedell@tulane.edu>
2021-09-23 19:43:41 -0400

    Comment out non-working code.

--- testmod/src/main/kotlin/Main.kt
+++ testmod/src/main/kotlin/Main.kt
@@ -12,18 +12,18 @@ data class MyData(
 fun main() {
     // Example using the generated idiomatic generic builder
     // for MyData.
-    val test = MyData.builder
-        .set(MyData.arg1, "test")
-        .set(MyData.arg2, 42)
-        .build()!!
-    println("Test complete: $test")
+    // val test = MyData.builder
+    //     .set(MyData.arg1, "test")
+    //     .set(MyData.arg2, 42)
+    //    .build()!!
+    // println("Test complete: $test")
 
     // Example using the buildable interface directly:
-    val test2 = with(MyData.buildable) {
-        empty
-            .combine(PartialMyData("test", null))
-            .combine(PartialMyData(null, 42))
-            .build()
-    }
-    println("Test complete: $test2")
+    // val test2 = with(MyData.buildable) {
+    //     empty
+    //         .combine(PartialMyData("test", null))
+    //         .combine(PartialMyData(null, 42))
+    //         .build()
+    // }
+    // println("Test complete: $test2")
 }
