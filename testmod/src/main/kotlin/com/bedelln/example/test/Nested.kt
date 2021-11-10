de79d38c4cc2220359df9d3fddf62f9bc2e83251
Nathan Bedell <nbedell@tulane.edu>
2021-11-10 08:34:32 -0500

    Add another test (@GenBuildable in nested scopes).

---
+++ testmod/src/main/kotlin/com/bedelln/example/test/Nested.kt
@@ -0,0 +1,20 @@
+
+package com.bedelln.example.test
+
+import com.bedelln.buildable.GenBuildable
+
+object Scope1 {
+    @GenBuildable
+    data class Nested(
+        val arg1: String,
+        val arg2: Int
+    )
+}
+
+class Scope2 {
+    @GenBuildable
+    data class Nested(
+       val arg1: Int,
+       val arg2: List<Pair<Double, Int>>
+    )
+}
